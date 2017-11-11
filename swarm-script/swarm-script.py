d3ff3ef9906228356acbdf5475e3da9daea0bc4e
boszerwp <wachirawit.bosz@mail.kmutt.ac.th>
2017-11-12 00:40:56 +0700

    add edit app_id#2

--- swarm-script/swarm-script.py
+++ swarm-script/swarm-script.py
@@ -47,7 +47,8 @@ class alumniSwarm:
         print(popen.communicate())
         return popen
 
-    def insert_deployed_stack_info(self,institute,array):
+    def insert_deployed_stack_info(self,institute,array,app_id):
+        app_id = app_id
         array = array.split(',')
         #print (array)
         ids = int(array[0])+1
@@ -61,7 +62,8 @@ class alumniSwarm:
             writer = csv.DictWriter(info,fieldnames=fn)
             writer.writerow({'id': ids,'name': institute,'api-port':api,'web-port':web,'db-port' : db})
             info.close()
-        ec = self.env_change(ins,api,web,db)   
+        ec = self.env_change(ins,api,web,db)
+        self.change_app_id(api,app_id)   
 
 
     
@@ -80,17 +82,17 @@ class alumniSwarm:
         self.deploy_stack(ins)
 
         
-    def last_line(self,institute):
+    def last_line(self,institute,app_id):
         ins = institute
         res = "bravo"
+        app_id = app_id
         count = 0
         with open('/var/www/manage_API/swarm-script/info.csv','r') as f:    
             for row in reversed(list(csv.reader(f))):
                 res = ','.join(row)
                 if(res != ''):
                     break
-        self.insert_deployed_stack_info(ins,res)
-
+        self.insert_deployed_stack_info(ins,res,app_id)
                     
     def replace_container(self,institute):
         shutil.copy('/var/www/manage_API/swarm-script/.env.example','/var/www/manage_API/swarm-script/.env')
@@ -115,26 +117,27 @@ class alumniSwarm:
         self.cp_file_container(institute)
         #self.replace_container(ins)
                     
-    def change_app_id(self,app_id):
-        shutil.copy('/var/www/Senior_Project/.env.example','/var/www/Senior_Project/.env')
-        with open('/var/www/Senior_Project/.env','r') as env:
+    def change_app_id(self,app_api,app_id):
+        shutil.copy('/var/www/Senior_Project/env.js.example','/var/www/Senior_Project/env.js')
+        with open('/var/www/Senior_Project/env.js','r') as env:
             s = env.read()
-        with open('/var/www/Senior_Project/.env','w') as env:
+        with open('/var/www/Senior_Project/env.js','w') as env:
             r = s.replace('app_id',str(app_id))
+            r = s.replace('app_api',str(app_api))
             env.write(r)
         
 
 
 
-    def deploy_infra(self,institute):
-        self.last_line(institute)
+    def deploy_infra(self,institute,app_id):
+        self.last_line(institute,app_id)
 
     def destroy_infra(self,institute):
         self.remove_stack(institute)
 
 
-alumniSwarm().deploy_infra(in_js[0].rstrip())
-alumniSwarm().change_app_id(in_js[1].rstrip())
+alumniSwarm().deploy_infra(in_js[0].rstrip(),in_js[1].rstrip())
+#alumniSwarm().change_app_id(in_js[1].rstrip())
 
 #alumniSwarm().destroy_infra(in_js[0].rstrip())
 
